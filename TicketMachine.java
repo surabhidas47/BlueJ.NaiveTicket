8d5d70f5bb063cef683447ca22caeb5260fd211e
Surabhi <surabhi@Zipcoders-MacBook-Pro-7.local>
2023-06-27 17:42:24 -0400

    weking

--- TicketMachine.java
+++ TicketMachine.java
@@ -19,7 +19,8 @@ public class TicketMachine
     private Integer total;
     // The number of tickets printed.
     private Integer ticketNumber;
-
+    
+    private Integer status;
     /**
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
